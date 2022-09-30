3158ef16de202a20603098c692563f8c25c58bde
Pedro Xavier <pedroxxavier@hotmail.com>
2022-09-30 11:00:07 -0300

    Consulta dinâmica e preenchimento automático.

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ async function buscaEndereco(cep) {
         if (consultaCEPConvertida.erro) {
             throw Error ('CEP não existente!');
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
         console.log(erro);
     }
 }
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value));
 
 // let ceps = ['01001000','01001001'];
 // let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
